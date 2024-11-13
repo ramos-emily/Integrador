be5dc0976ac407247a88c246c4bc98b3c8a3569c
Emily Vit�ria de Souza Ramos <42749126800@>
2024-11-13 16:58:41 -0300

    atualizando fotos e database

--- scripts/database.js
+++ scripts/database.js
@@ -1,35 +1,34 @@
 export const database = [ /* "Banco de dados" de produtos comercializados no site */
-    // {
-    //     codigoProduto:1,
-    //     tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
-    //     preco: 400.00, 
-    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
-    //     imagemProduto: {
-    //         img1:"./images/products/collan_preto.png",
-    //         img2:"./images/products/collan_rosa.png",
-    //         img3:"./images/products/collan_pink.png",
-    //         img4:"./images/products/collan_vermelho.png",
-
-    //     },
-    //     categoriaProduto: 'collans',
-    //     classificacaoProduto:"Novidades",
-    //     exibirHome: true
-    // },
-    // {
-    //     codigoProduto:2,
-    //     tituloProduto:"Collant - Pink - Adulto FP711 PROMOCIONAL", 
-    //     preco: 300.00, 
-    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
-    //     imagemProduto: {
-    //         img1:"./images/products/collan_rosa.png",
-    //         img2:"./images/products/prod02/img2.png",
-    //         img3:"./images/products/prod02/img3.png",
-    //         img4:"./images/products/prod02/img4.png",
-    // },
-    //     categoriaProduto: 'Ferramentas',
-    //     classificacaoProduto:"Novidades",
-    //     exibirHome: true
-    // },
+    {
+        codigoProduto:1,
+        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
+        preco: 400.00, 
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+        imagemProduto: {
+            img1:"./images/products/collan_preto.png",
+            img2:"./images/products/collan_rosa.png",
+            img3:"./images/products/collan_pink.png",
+            img4:"./images/products/collan_vermelho.png",
+        },
+        categoriaProduto: 'collans',
+        classificacaoProduto:"Novidades",
+        exibirHome: false
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Collant - Pink - Adulto FP711 PROMOCIONAL", 
+        preco: 300.00, 
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+        imagemProduto: {
+            img1:"./images/products/collan_rosa.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+    },
+        categoriaProduto: 'collans',
+        classificacaoProduto:"Novidades",
+        exibirHome: false
+    },
     {
         codigoProduto:3,
         tituloProduto:"Collant - Rosa - Adulto FP711 PROMOCIONAL", 
@@ -41,62 +40,62 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'collans',
         classificacaoProduto:"Novidades",
         exibirHome: false
     },
-    // {
-    //     codigoProduto:4,
-    //     tituloProduto:"Collant - Rosa - Adulto FP711 PROMOCIONAL", 
-    //     preco: 300.00, 
-    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
-    //     imagemProduto: {
-    //         img1:"./images/products/collan_pink.png",
-    //         img2:"./images/products/prod02/img2.png",
-    //         img3:"./images/products/prod02/img3.png",
-    //         img4:"./images/products/prod02/img4.png",
-
-    //     },
-    //     categoriaProduto: 'Ferramentas',
-    //     classificacaoProduto:"Promocoes",
-    //     exibirHome: true
-    // },
     {
-        codigoProduto:5,
-        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
+        codigoProduto:4,
+        tituloProduto:"Collant - Rosa - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
         descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
-            img1:"./images/products/collan_vermelho.png",
+            img1:"./images/products/collan_pink.png",
             img2:"./images/products/prod02/img2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'collans',
+        classificacaoProduto:"Promocoes",
+        exibirHome:false
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"Collant - Rosa - Adulto FP711 PROMOCIONAL", 
+        preco: 399.00, 
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+        imagemProduto: {
+            img1:"./images/products/collan_rosa.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'collans',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
     {
         codigoProduto:6,
-        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
-        preco: 300.00, 
+        tituloProduto:"Collant - Pink - Adulto FP711 PROMOCIONAL", 
+        preco: 499.00, 
         descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
-            img1:"./images/products/collan_vermelho.png",
+            img1:"./images/products/collan_pink.png",
             img2:"./images/products/prod02/img2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'collans',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
     {
         codigoProduto:7,
-        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
-        preco: 300.00, 
+        tituloProduto:"Collant - Vermelho - Adulto FP711 PROMOCIONAL", 
+        preco: 320.90, 
         descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
             img1:"./images/products/collan_vermelho.png",
@@ -105,29 +104,29 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
             img4:"./images/products/prod02/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'collans',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
-    {
-        codigoProduto:8,
-        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
-        preco: 300.00, 
-        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
-        imagemProduto: {
-            img1:"./images/products/collan_vermelho.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+    // {
+    //     codigoProduto:8,
+    //     tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
+    //     preco: 300.00, 
+    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+    //     imagemProduto: {
+    //         img1:"./images/products/collan_vermelho.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
 
-        },
-        categoriaProduto: 'Ferramentas',
-        classificacaoProduto:"Mais_Vendidos",
-        exibirHome: true
-    },
+    //     },
+    //     categoriaProduto: 'collans',
+    //     classificacaoProduto:"Mais_Vendidos",
+    //     exibirHome: true
+    // },
     {
         codigoProduto:9,
-        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
+        tituloProduto:"Collant - Preto - Adulto FP714 PROMOCIONAL", 
         preco: 300.00, 
         descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
@@ -137,7 +136,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
             img4:"./images/products/prod02/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'collans',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
