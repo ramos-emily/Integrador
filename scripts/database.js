f97a56418959c6cec8900a73f988db48c682c919
Emily <ct67ca@bosch.com>
2024-11-13 10:42:41 -0300

    organizei carrousel

--- scripts/database.js
+++ scripts/database.js
@@ -1,72 +1,71 @@
 export const database = [ /* "Banco de dados" de produtos comercializados no site */
-    {
-        codigoProduto:1,
-        tituloProduto:"Furadeira e Parafusadeira de Impacto a Bateria Bosch com Carregador e Bateria 18V 1/2 GSB 180-Li Bivolt", 
-        preco: 620.00, 
-        descricao:"Para furar madeiras, metais e alvenaria, a Furadeira e Parafusadeira GSB 180-Li é a solução ideal! Com o maior torque da categoria, baixa manutenção e a maior resistência se comparar com os principais concorrentes, é sinônimo de máxima performance com o melhor custo benefício.",
-        imagemProduto: {
-            img1:"./images/products/collan_preto.png",
-            img2:"./images/products/collan_rosa.png",
-            img3:"./images/products/collan_pink.png",
-            img4:"./images/products/collan_vermelho.png",
+    // {
+    //     codigoProduto:1,
+    //     tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
+    //     preco: 400.00, 
+    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+    //     imagemProduto: {
+    //         img1:"./images/products/collan_preto.png",
+    //         img2:"./images/products/collan_rosa.png",
+    //         img3:"./images/products/collan_pink.png",
+    //         img4:"./images/products/collan_vermelho.png",
 
-        },
-        categoriaProduto: 'collans',
-        classificacaoProduto:"Novidades",
-        exibirHome: true
-    },
-    {
-        codigoProduto:2,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
-        imagemProduto: {
-            img1:"./images/products/collan_rosa.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
-    },
-        categoriaProduto: 'Ferramentas',
-        classificacaoProduto:"Novidades",
-        exibirHome: true
-    },
+    //     },
+    //     categoriaProduto: 'collans',
+    //     classificacaoProduto:"Novidades",
+    //     exibirHome: true
+    // },
+    // {
+    //     codigoProduto:2,
+    //     tituloProduto:"Collant - Pink - Adulto FP711 PROMOCIONAL", 
+    //     preco: 300.00, 
+    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+    //     imagemProduto: {
+    //         img1:"./images/products/collan_rosa.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
+    // },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Novidades",
+    //     exibirHome: true
+    // },
     {
         codigoProduto:3,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"Collant - Rosa - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
-            img1:"./images/products/collan_vermelho.png",
+            img1:".",
             img2:"./images/products/prod02/img2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
-
         },
         categoriaProduto: 'Ferramentas',
         classificacaoProduto:"Novidades",
         exibirHome: false
     },
-    {
-        codigoProduto:4,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
-        imagemProduto: {
-            img1:"./images/products/collan_pink.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+    // {
+    //     codigoProduto:4,
+    //     tituloProduto:"Collant - Rosa - Adulto FP711 PROMOCIONAL", 
+    //     preco: 300.00, 
+    //     descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
+    //     imagemProduto: {
+    //         img1:"./images/products/collan_pink.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
 
-        },
-        categoriaProduto: 'Ferramentas',
-        classificacaoProduto:"Promocoes",
-        exibirHome: true
-    },
+    //     },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Promocoes",
+    //     exibirHome: true
+    // },
     {
         codigoProduto:5,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
             img1:"./images/products/collan_vermelho.png",
             img2:"./images/products/prod02/img2.png",
@@ -80,9 +79,9 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:6,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
             img1:"./images/products/collan_vermelho.png",
             img2:"./images/products/prod02/img2.png",
@@ -96,9 +95,9 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:7,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
             img1:"./images/products/collan_vermelho.png",
             img2:"./images/products/prod02/img2.png",
@@ -112,9 +111,9 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:8,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
             img1:"./images/products/collan_vermelho.png",
             img2:"./images/products/prod02/img2.png",
@@ -128,9 +127,9 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:9,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        tituloProduto:"Collant - Preto - Adulto FP711 PROMOCIONAL", 
         preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        descricao:"Collant macio e com a melhor qualidade para você entregar seu melhor na dança",
         imagemProduto: {
             img1:"./images/products/collan_preto.png",
             img2:"./images/products/prod02/img2.png",
