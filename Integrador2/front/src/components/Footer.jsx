fe9ce2a98b2830a8910e4c5943976969a567cc7e
ramos-emily <ramos.emilyvi@gmail.com>
2025-05-28 14:18:35 -0300

    tirando integrador daqui

--- Integrador2/front/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export function Footer() {
-    return (
-        <footer className="flex items-center justify-center !p-2 text-[#3473BA] text-[16px] font-bold bg-white
-        sm:text-[14px]">
-            Integrador 2025
-        </footer>
-    )
-}
