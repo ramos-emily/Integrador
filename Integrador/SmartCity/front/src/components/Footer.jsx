5a9a32e2d18269ef2dd7046bfa3a5fe18ac4daa0
ramos-emily <ramos.emilyvi@gmail.com>
2025-07-04 09:12:30 -0300

    refatorando pastas

--- Integrador/SmartCity/front/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export function Footer() {
-    return (
-        <footer className="flex items-center justify-center !p-2 text-[#226D13] text-[16px] font-bold bg-white
-        sm:text-[14px]">
-            Integrador 2025
-        </footer>
-    )
-}
